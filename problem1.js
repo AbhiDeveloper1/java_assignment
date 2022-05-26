7615233e66c29703dcca5b1cdfaf57aa9f86876a
Abhishek Khandal <>
2022-05-26 12:44:02 +0000

    Initial commit

---
+++ problem1.js
@@ -0,0 +1,6 @@
+var i=prompt("enter number to find factorial");
+var factorial=1;
+for(let j=1;j<=i;j++){
+  factorial=factorial*j;
+}
+console.log(`Factorial is ${factorial}`);
---
+++ problem2.js
@@ -0,0 +1,7 @@
+var i=prompt("enter number to find cube");//3
+var cube=1;
+for(let j=0;j<=2;j++)
+  {
+    cube=cube*i;
+  }
+console(`Cube is:${cube}`)
